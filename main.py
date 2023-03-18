43b79eae004886f6766f35c6d34164edd859af6d
Andrew Aires <121694498+andrewaires@users.noreply.github.com>
2023-03-17 23:38:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# selenium 4
+# Importa a versão mais recente do Selenium para usar com Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
 import time
 from selenium.webdriver.common.keys import Keys
 
-# aqui começa o bot
 class bot_instagram:
     def __init__(self):
         serv = ChromeService(executable_path=ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=serv)
     
-    # seleciona qual perfil entrar
+    # Seleciona qual perfil entrar
     def entrar(self, link):
         self.driver.get(link)
 
-    # seleciona o link das fotos do perfil
+    # Seleciona o link das fotos do perfil
     def pegar_link(self):
         links = self.driver.find_elements(by='tag name', value='a')
         lista = []
@@ -25,7 +25,7 @@ class bot_instagram:
                 lista.append(href)
         return lista
 
-    # comenta nas fotos
+    # Comenta nas fotos
     def comentar(self, comentario):
         textarea = self.driver.find_element(by='css selector', value='textarea[aria-label="Adicione um comentário..."]')
         time.sleep(1)
@@ -40,20 +40,20 @@ class bot_instagram:
         
 
 bot = bot_instagram()
-# acessa a página principal do instagram
+# Acessa a página principal do instagram
 bot.entrar('https://www.instagram.com/')
-# espera 15s para você realizar o login
-time.sleep(15)
-# acessa o perfil
+# Espera 18s para você realizar o login
+time.sleep(18)
+# Acessa o perfil
 bot.entrar('https://www.instagram.com/forbesbr/')
 time.sleep(2)
 fotos = bot.pegar_link()
-print(fotos)
 
 for foto in fotos:
     bot.entrar(foto)
     bot.driver.execute_script('return document.readyState')
     time.sleep(4)
-    # comenta nas fotas
+    
+    # Comenta nas fotas
     bot.comentar('comentário teste')
     time.sleep(2)
